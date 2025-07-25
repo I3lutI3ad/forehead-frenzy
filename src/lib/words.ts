132ae448c224e53b4fb38d18f51fdc3ce326d74e
Ripu Sudan <ripu.sudan@trantorinc.com>
2025-07-25 04:54:49 +0000

    take some time and add atleast 50 words in each category

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -3,11 +3,20 @@ export const wordCategories: Record<string, Record<string, string[]>> = {
   "Animals": {
     "General": [
       "Elephant", "Tiger", "Giraffe", "Zebra", "Monkey", "Lion", "Hippo", 
-      "Kangaroo", "Penguin", "Dolphin", "Koala", "Panda", "Cheetah", "Gorilla", "Octopus"
+      "Kangaroo", "Penguin", "Dolphin", "Koala", "Panda", "Cheetah", "Gorilla", "Octopus",
+      "Wolf", "Fox", "Bear", "Deer", "Rabbit", "Squirrel", "Otter", "Beaver", "Raccoon", "Skunk", 
+      "Moose", "Eagle", "Hawk", "Owl", "Falcon", "Vulture", "Snake", "Lizard", "Turtle", "Frog", 
+      "Crocodile", "Alligator", "Shark", "Whale", "Bat", "Butterfly", "Bee", "Ant", "Spider", 
+      "Scorpion", "Crab", "Lobster", "Shrimp", "Jellyfish", "Starfish", "Horse", "Cow", "Pig", 
+      "Sheep", "Chicken", "Goat", "Turkey", "Duck", "Peacock", "Swan"
     ],
     "भारतीय और हिंदी": [
-      "मोर", "बाघ", "लंगूर", "कोबरा", "सांभर", "नेवला", "नीलगाय",
-      "घड़ियाल", "शेर", "हाथी", "बंदर", "गाय", "ऊंट", "भालू", "मगरमच्छ"
+      "मोर", "बाघ", "लंगूर", "कोबरा", "सांभर", "नेवला", "नीलगाय", "घड़ियाल", "शेर", "हाथी", "बंदर", 
+      "गाय", "ऊंट", "भालू", "मगरमच्छ", "तेंदुआ", "लकड़बग्घा", "गैंडा", "दरियाई घोड़ा", "कंगारू",
+      "लोमड़ी", "हिरण", "खरगोश", "गिलहरी", "उल्लू", "बाज", "गिद्ध", "साँप", "छिपकली", "कछुआ", 
+      "मेंढक", "चमगादड़", "तितली", "मधुमक्खी", "चींटी", "मकड़ी", "बिच्छू", "केकड़ा", "झींगा",
+      "घोड़ा", "सुअर", "भेड़", "बकरी", "मुर्गी", "बत्तख", "हंस", "कबूतर", "तोता", "मछली",
+      "कुत्ता", "बिल्ली", "चूहा", "सियार", "भैंस", "बैल", "याक", "गौर", "भेड़िया"
     ]
   },
   "TV Shows and Movies": {
@@ -16,37 +25,77 @@ export const wordCategories: Record<string, Record<string, string[]>> = {
       "The Crown", "Money Heist", "Black Mirror", "The Simpsons", "Seinfeld", 
       "Westworld", "The Mandalorian", "Sherlock", "Doctor Who", "Peaky Blinders",
       "Inception", "The Dark Knight", "Forrest Gump", "Pulp Fiction", "The Matrix",
-      "Avatar", "Titanic", "Jurassic Park", "Star Wars", "The Avengers"
+      "Avatar", "Titanic", "Jurassic Park", "Star Wars", "The Avengers", "The Godfather",
+      "Schindler's List", "The Shawshank Redemption", "The Lord of the Rings", "Harry Potter",
+      "Finding Nemo", "Toy Story", "The Lion King", "Frozen", "Interstellar", "Gladiator",
+      "Braveheart", "Fight Club", "Goodfellas", "The Silence of the Lambs", "Parasite",
+      "Joker", "Squid Game", "The Witcher", "Narcos", "Vikings", "House of Cards", 
+      "Orange Is the New Black", "Chernobyl", "Fleabag", "The Sopranos", "Wire", 
+      "Arrested Development", "Curb Your Enthusiasm", "Succession"
     ],
     "भारतीय और हिंदी": [
       "CID", "तारक मेहता", "महाभारत", "रामायण", "सेक्रेड गेम्स", "मिर्जापुर",
       "द फैमिली मैन", "पंचायत", "स्कैम 1992", "बिग बॉस", "KBC", "शक्तिमान",
       "साराभाई vs साराभाई", "लगान", "3 इडियट्स", "दंगल", "बाहुबली", "RRR",
-      "शोले", "मुग़ल-ए-आज़म", "क्वीन", "अंधाधुन", "चक दे इंडिया"
+      "शोले", "मुग़ल-ए-आज़म", "क्वीन", "अंधाधुन", "चक दे इंडिया", "गंगाजल", "सरफरोश",
+      "पान सिंह तोमर", "बजरंगी भाईजान", "गोलमाल", "हेरा फेरी", "वेलकम", "धमाल",
+      "बॉर्डर", "दिलवाले दुल्हनिया ले जाएंगे", "कुछ कुछ होता है", "कभी खुशी कभी ग़म",
+      "रंग दे बसंती", "क्राइम पेट्रोल", "इंडियन आइडल", "कॉमेडी नाइट्स", "कपिल शर्मा शो",
+      "देवों के देव महादेव", "नागिन", "ये रिश्ता क्या कहलाता है", "अनुपमा", "चंद्रकांता",
+      "व्योमकेश बख्शी", "देख भाई देख", "ऑफिस ऑफिस", "हम लोग", "बुनियाद", "फ्लॉप शो",
+      "आश्रम", "एस्पिरेंट्स", "गुल्लक"
     ]
   },
   "Celebrities": {
     "General": [
       "Beyoncé", "Tom Hanks", "Dwayne Johnson", "Taylor Swift", "Leonardo DiCaprio", 
       "Jennifer Lawrence", "Will Smith", "Ariana Grande", "Keanu Reeves", "Rihanna", 
-      "Brad Pitt", "Angelina Jolie", "Oprah Winfrey", "Elon Musk", "Serena Williams"
+      "Brad Pitt", "Angelina Jolie", "Oprah Winfrey", "Elon Musk", "Serena Williams",
+      "Tom Cruise", "Meryl Streep", "Johnny Depp", "Robert Downey Jr.", "Chris Hemsworth",
+      "Scarlett Johansson", "Mark Zuckerberg", "Jeff Bezos", "Bill Gates", "Kanye West",
+      "Kim Kardashian", "Justin Bieber", "Selena Gomez", "Lady Gaga", "Adele", "Ed Sheeran",
+      "Cristiano Ronaldo", "Lionel Messi", "LeBron James", "Michael Jordan", "Tiger Woods",
+      "Usain Bolt", "Stephen King", "J.K. Rowling", "Steven Spielberg", "Quentin Tarantino",
+      "Martin Scorsese", "Christopher Nolan", "Tim Cook", "Dalai Lama", "Pope Francis",
+      "Queen Elizabeth II", "Barack Obama", "Donald Trump", "Joe Biden", "Vladimir Putin"
     ],
     "भारतीय और हिंदी": [
       "शाहरुख खान", "अमिताभ बच्चन", "प्रियंका चोपड़ा", "दीपिका पादुकोण", "सलमान खान",
       "आमिर खान", "सचिन तेंदुलकर", "विराट कोहली", "ए. आर. रहमान", "लता मंगेशकर",
-      "नरेन्द्र मोदी", "रजनीकांत", "अक्षय कुमार", "करीना कपूर", "ह्रितिक रोशन"
+      "नरेन्द्र मोदी", "रजनीकांत", "अक्षय कुमार", "करीना कपूर", "ह्रितिक रोशन",
+      "रणबीर कपूर", "आलिया भट्ट", "अनुष्का शर्मा", "माधुरी दीक्षित", "श्रीदेवी",
+      "धर्मेन्द्र", "हेमा मालिनी", "रेखा", "अनिल कपूर", "कमल हासन", "चिरंजीवी",
+      "महेंद्र सिंह धोनी", "सौरव गांगुली", "कपिल देव", " मिल्खा सिंह", "पी. वी. सिंधु",
+      "Mary Kom", "Sania Mirza", "Mukesh Ambani", "Ratan Tata", "Azim Premji",
+      "Kailash Satyarthi", "Amartya Sen", "C.V. Raman", "Satya Nadella", "Sundar Pichai",
+      "Shreya Ghoshal", "Arijit Singh", "Sonu Nigam", "Kishore Kumar", "Mohammed Rafi",
+      "Zakir Hussain", "Pandit Ravi Shankar", "Javed Akhtar", "Gulzar", "Prasoon Joshi"
     ]
   },
   "Places": {
     "General": [
-        "Paris", "New York", "London", "Tokyo", "Sydney", "Rome", "Cairo", 
-        "Rio de Janeiro", "Moscow", "Dubai", "Grand Canyon", "Mount Everest", 
-        "Eiffel Tower", "Statue of Liberty", "Great Wall of China"
+      "Paris", "New York", "London", "Tokyo", "Sydney", "Rome", "Cairo", 
+      "Rio de Janeiro", "Moscow", "Dubai", "Grand Canyon", "Mount Everest", 
+      "Eiffel Tower", "Statue of Liberty", "Great Wall of China", "Machu Picchu",
+      "Pyramids of Giza", "Colosseum", "Venice", "Barcelona", "Amsterdam",
+      "Berlin", "Prague", "Santorini", "Maldives", "Bora Bora", "Hawaii",
+      "Las Vegas", "Los Angeles", "San Francisco", "Chicago", "Toronto",
+      "Vancouver", "Mexico City", "Bangkok", "Singapore", "Hong Kong",
+      "Seoul", "Beijing", "Jerusalem", "Mecca", "Vatican City", "Istanbul",
+      "Amazon Rainforest", "Sahara Desert", "Antarctica", "Arctic Circle",
+      "Niagara Falls", "Victoria Falls", "Iguazu Falls", "Angel Falls",
+      "Serengeti", "Galapagos Islands", "Stonehenge"
     ],
     "भारतीय और हिंदी": [
-        "ताज महल", "दिल्ली", "मुंबई", "गोवा", "जयपुर", "वाराणसी", "हिमालय", 
-        "केरल", "अमृतसर", "लद्दाख", "कुतुब मीनार", "लाल किला", "इंडिया गेट", 
-        "गेटवे ऑफ इंडिया", "मैसूर पैलेस"
+      "ताज महल", "दिल्ली", "मुंबई", "गोवा", "जयपुर", "वाराणसी", "हिमालय", 
+      "केरल", "अमृतसर", "लद्दाख", "कुतुब मीनार", "लाल किला", "इंडिया गेट", 
+      "गेटवे ऑफ इंडिया", "मैसूर पैलेस", "हम्पी", "खजुराहो", "अजंता गुफाएं",
+      "एलोरा गुफाएं", "कोणार्क सूर्य मंदिर", "शिमला", "मनाली", "ऊटी", "दार्जिलिंग",
+      "ऋषिकेश", "हरिद्वार", "तिरुपति", "वैष्णो देवी", "अमरनाथ", "केदारनाथ",
+      "बद्रीनाथ", "रामेश्वरम", "कन्याकुमारी", "अंडमान द्वीप समूह", "लक्षद्वीप",
+      "सुंदरबन", "थार मरुस्थल", "कच्छ का रण", "चिल्का झील", "लोनार झील",
+      "नैनीताल", "आगरा", "जोधपुर", "उदयपुर", "जैसलमेर", "पुष्कर",
+      "औरंगाबाद", "हैदराबाद", "चेन्नई", "कोलकाता", "बेंगलुरु", "पुणे"
     ]
   },
   "Mixed": {
@@ -72,7 +121,9 @@ wordCategories.Mixed['भारतीय और हिंदी'] = [
 export const getWordsForCategory = (category: string, variant: string): string[] => {
   const categoryVariants = wordCategories[category];
   if (categoryVariants) {
-    return categoryVariants[variant] || [];
+    const words = categoryVariants[variant] || [];
+    // Shuffle the array to make it random every time
+    return words.sort(() => Math.random() - 0.5);
   }
 
   return [];
